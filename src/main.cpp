b73d49749d317318e66d235e8af7fe571350667e
gsanhueza <gabriel_8032@hotmail.com>
2017-04-24 14:50:14 -0300

    initial project import from KDevelop

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "tarea_2.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Tarea_2 w;
+    w.show();
+
+    return app.exec();
+}
+
